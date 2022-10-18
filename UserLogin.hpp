cddeae497976723fa67728e7fae473e6627e16fa
giatung2002 <70301943+giatung2002@users.noreply.github.com>
2022-10-17 22:37:00 -0700

    User Login

---
+++ UserLogin.hpp
@@ -0,0 +1,100 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+class UserLogin {
+private:
+
+    unordered_map< string, string > table;
+
+
+public:
+    UserLogin(); // default constructor
+
+    void readIn(const string& filename); // add data from file to hash table
+
+    size_t numberOfUsers(); // return the number of users
+
+    string passWordCheck(const string& userName); // returns the passWord of the given user
+
+    size_t wordBucketIdMethod1(const string& userName); // return the bucket number where the given user is located
+
+    size_t wordBucketIdMethod2(const string& userName); // another way to find the bucket number where the given user is located
+
+    bool validateUser(const string& userName); // look up given user
+
+    bool authenticateUser(const string& userName, const string& passWord);// authenticate given user and password
+
+};
+
+UserLogin::UserLogin() {
+}
+
+// COMPLETE THE FOLLOWING FUNCTIONS
+
+void UserLogin::readIn(const string& filename) {
+  ifstream fin;
+  fin.open(filename);
+  if(!fin.is_open())
+  {
+    return;
+  }
+  string line;
+  while(getline(fin,line))
+  {
+    stringstream ss(line);
+
+    string userName, password;
+    ss >> userName >> password;
+    table[userName]=password;
+  }
+}
+
+    // TO DO
+
+size_t UserLogin::numberOfUsers() {
+  return table.size();
+    // TO DO
+
+}
+
+string UserLogin::passWordCheck(const string& userName) {
+  return table.find(userName)!=table.end()?table[userName]:"Non-existent User";
+    // TO DO
+
+}
+
+size_t UserLogin::wordBucketIdMethod1(const string& userName) {
+  return table.bucket(userName);
+    // TO DO
+
+}
+
+size_t UserLogin::wordBucketIdMethod2(const string& userName) {
+  // TO DO
+  unsigned n = table.bucket_count();
+
+  for(unsigned i = 0; i < n; i++) {
+    for(auto it = table.begin(i);it!=table.end(i);++it)
+      if(it->first==userName)
+        return i;
+
+  }
+  return 0;
+}
+
+
+bool UserLogin::validateUser(const string& userName) {
+  return table.find(userName)!=table.end();
+    // TO DO
+}
+
+bool UserLogin::authenticateUser(const string& userName, const string& passWord){
+  
+}
+    // TO DO
